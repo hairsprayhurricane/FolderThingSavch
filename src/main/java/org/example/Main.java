450ede7130c7ef1c4b40f314f78bfae8358e9976
savch <savchenko.zahar@gmail.com>
2024-05-03 14:59:34 +0300

    .

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.io.File;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main
+{
+    //Савченко
+    public static void main(String[] args)
+    {
+        ParametersBag bag = new ParametersBag(args);
+
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
+
+        File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
+
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator =
+            new FolderSizeCalculator(root);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(calculator);
+
+        System.out.println(root);
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
+    }
+}
